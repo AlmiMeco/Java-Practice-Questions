8f44ab8c261b0dfb294028f3924492fb82703b78
Almi's PC <almimeci@gmail.com>
2023-05-06 18:34:16 -0500

    LEET #205 Iso-Strings
    FINISHED!!!

--- src/main/java/IsomorphicStrings_205.java
+++ src/main/java/IsomorphicStrings_205.java
@@ -17,23 +17,25 @@ public class IsomorphicStrings_205 {
 
     public static boolean isIsomorphic(String s, String t) {
 
-        Set<Character> hashChk = new HashSet<>();
+        int[] arrayOfS = new int[200];
+        int[] arrayOfT = new int[200];
 
         if (s.length() != t.length()) return false;
 
         for (int i = 0; i < s.length(); i++) {
-            if (!hashChk.contains(s.charAt(i))) {
-                hashChk.add(s.charAt(i));
 
-            }else return false;
+            if (arrayOfS[s.charAt(i)] != arrayOfT[t.charAt(i)]) return false;
+
+            arrayOfS[s.charAt(i)]=i+1;
+            arrayOfT[t.charAt(i)]=i+1;
         }
         return true;
     }
 
     public static void main(String[] args) {
 
-        String s = "egg";
-        String t = "add";
+        String s = "paper";
+        String t = "title";
 
 
         System.out.println(isIsomorphic(s, t));
