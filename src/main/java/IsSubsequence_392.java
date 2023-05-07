b32a05b754f14caa741d4931b05f9c4ef0b398b8
Almi's PC <almimeci@gmail.com>
2023-05-07 01:48:03 -0500

    LEET #392 Is-Subsequence
    FINISHED!! (- the -> "disturbing the relative positions" )

---
+++ src/main/java/IsSubsequence_392.java
@@ -0,0 +1,46 @@
+public class IsSubsequence_392 {
+
+/** Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+ A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the
+ characters without disturbing the relative positions of the remaining characters.
+ (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+ Input: s = "abc", t = "ahbgdc"
+ Output: true
+
+ **/
+
+
+
+
+    public static boolean isSubsequence(String s, String t) {
+
+        Character placeHolderChar = null;
+
+        for (int i = 0; i < s.length(); i++) {
+            placeHolderChar = s.charAt(i);
+            CharSequence character = placeHolderChar.toString();
+
+            if (t.contains(character)) {
+
+            }else return false;
+
+        }
+
+        return true;
+
+    }
+
+
+    public static void main(String[] args) {
+
+        String s = "abc";
+        String t = "ahbgdc";
+
+        System.out.println(isSubsequence(s,t));
+
+
+    }
+
+}
